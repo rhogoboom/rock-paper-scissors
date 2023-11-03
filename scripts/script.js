51631a664193b15bb8a635832c23fb40ae09d790
Richard Hogoboom <rhogoboom@gmail.com>
2023-11-02 20:00:47 -0400

    implemented a single round of the game

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,11 @@ function getComputerChoice() {
     let choice_int = getRandomInt();
     switch (choice_int) {
         case 0:
-            return 'Rock';
+            return 'rock';
         case 1:
-            return 'Paper';
+            return 'paper';
         case 2:
-            return 'Scissors';
+            return 'scissors';
         default:
             return
 
@@ -18,3 +18,73 @@ function getRandomInt() {
     return Math.floor(Math.random() * max);
 }
 
+
+
+function cleanChoice(choice) {
+    return choice.toLowerCase().trim();
+}
+
+function isValidChoice(choice) {
+    const validChoices = ['rock', 'paper', 'scissors'];
+    return validChoices.includes(choice);
+}
+
+function decideWinScenario(playerSelection, computerSelection) {
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return 0;
+        } else if (computerSelection === 'scissors') {
+            return 1;
+        } else {
+            return 2;
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return 0;
+        } else if (computerSelection === 'rock') {
+            return 1;
+        } else {
+            return 2;
+        }
+    } else {
+        if (computerSelection === 'rock') {
+            return 0;
+        } else if (computerSelection === 'paper') {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
+
+function makeOutputString(winScenario, playerSelection, computerSelection) {
+    const capitalizePlayer = capitalizeFirstLetter(playerSelection);
+    const capitalizeComputer = capitalizeFirstLetter(computerSelection);
+    switch (winScenario) {
+        case 0:
+
+            return `You Lose! ${capitalizeComputer} beats ${capitalizePlayer}.`
+        case 1:
+            return `You Win! ${capitalizePlayer} beats ${capitalizeComputer}.`
+        case 2:
+            return `You Tie! You both chose ${capitalizePlayer}.`
+        default:
+            break
+    }
+
+}
+
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function playRound(playerSelection, computerSelection) {
+    const cleanedChoice = cleanChoice(playerSelection);
+    if (isValidChoice(cleanedChoice)) {
+        let winScenario = decideWinScenario(cleanedChoice, computerSelection);
+        let outputString = makeOutputString(winScenario, cleanedChoice, computerSelection);
+        return outputString;
+
+    }
+
+}
